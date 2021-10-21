a9ce52a5cc52814c95503040796288b1599aac98
cicero884 <joy861225@gmail.com>
2021-10-21 22:29:54 +0800

    add assembly file and optomize c code

--- hw1/main.c
+++ hw1/main.c
@@ -6,34 +6,35 @@ int input_data[MAX_ARRAY];
 int input_size;
 _Bool isMonotonic(int* nums, int numsSize){
 	if(numsSize<3) return true;
-	int i=1,prev=nums[0],now;
-	_Bool type=prev>nums[numsSize-1];
-	if(type){
+	int i=1,prev=nums[0],now=nums[1];
+	_Bool result=false;
+	if(prev>nums[numsSize-1]){
 		while(i<numsSize){
-			now=nums[i];
-			if(prev < now) return false;
+			result|=(prev < now);
 			prev=now;
 			++i;
+			now=nums[i];
 		}
 	}
 	else{
 		while(i<numsSize){
-			now=nums[i];
-			if(prev > now) return false;
+			result|=(prev > now);
 			prev=now;
 			++i;
+			now=nums[i];
 		}
 	}
-	return true;
+	return !result;
 }
 
 int main(){
 	//for(input_size=0;input_size<MAX_ARRAY && scanf("%d",&input_data[input_size])!=EOF; ++input_size);
 	//for(int i=0;i<input_size;++i) printf("%d ",input_data[i]);
-	input[0]=1;
-	input[2]=2;
-	input[3]=2;
-	input[4]=1;
+	input_data[0]=1;
+	input_data[1]=2;
+	input_data[2]=2;
+	input_data[3]=1;
+	input_size=4;
 
 	if(isMonotonic(input_data,input_size)) printf("true");
 	else printf("false");
